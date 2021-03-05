b80a7b52c81d4b81dbac4757379db4341856d93b
丁松杰 <377973147@qq.com>
2021-03-05 18:16:37 +0800

    fix format

--- src/Pole.ReliableMessage.Core/Abstraction/IRetryTimeDelayCalculator.cs
+++ src/Pole.ReliableMessage.Core/Abstraction/IRetryTimeDelayCalculator.cs
@@ -1,4 +1,4 @@
-﻿opyright(c).NET Core Community.All rights reserved.
+﻿//opyright(c).NET Core Community.All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
